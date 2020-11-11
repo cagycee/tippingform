8cb0faeef4794c44f1ab8c7fae86f9c1dc8e67b6
cagycee <71255514+cagycee@users.noreply.github.com>
2020-11-10 21:47:46 -0600

    adding more files/renaming

---
+++ js/file1.js
@@ -0,0 +1,9 @@
+var billTotal;
+const caculateTip = (total, perc) => {
+
+};
+
+const checkJavascript = () => {
+    document.getElementById('js').innerHTML="Javscript is Working!";
+    document.getElementById('js').style="color: green;";  
+};
