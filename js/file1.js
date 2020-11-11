47936b3962785990c15581032b9404607e7d96fd
cagycee <71255514+cagycee@users.noreply.github.com>
2020-11-10 22:38:46 -0600

    additional content

--- js/file1.js
+++ js/file1.js
@@ -1,7 +1,17 @@
 var billTotal;
-const caculateTip = (total, perc) => {
-
+const calculateTip = (perc) => {
+    billTotal = document.getElementById("tipvalue").value;
+    if (billTotal > 0) {
+        var tip = Math.round((billTotal * perc * 100)) / 100;
+        alert(`You should tip ${tip}!`);
+    } else {
+        alert('Bill Total Value is empty!');
+    }
 };
+
+const custom = () => {
+    
+}
 
 const checkJavascript = () => {
     document.getElementById('js').innerHTML="Javscript is Working!";
